94e7c2471f07fbc11d7903396cfa90fb0efe29cf
arnold <arnold@robolink.com>
2020-06-22 14:25:36 -0700

    new classifier

--- wifi_connect.py
+++ wifi_connect.py
@@ -2,6 +2,8 @@ import zumidashboard.scripts as scripts
 #add your info in here then run this program
 network_name = ""
 network_password = ""
+# network_name = "my_network"
+# network_password = "my_password"
 
 scripts.get_ssid_list()
 scripts.kill_supplicant()
